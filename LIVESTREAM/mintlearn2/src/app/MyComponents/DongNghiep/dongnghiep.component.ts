41cb7da522791a15c1c8538a1972801fabe65dc2
toan truong <truongvantoan.neu@gmail.com>
2018-12-04 22:46:27 +0700

    add

---
+++ LIVESTREAM/mintlearn2/src/app/MyComponents/DongNghiep/dongnghiep.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'dongnghiep',
+    template: `
+    <h1>Chao cac ban !!! </h1>
+    `,
+})
+
+export class DongnghiepComponent{
+
+}
