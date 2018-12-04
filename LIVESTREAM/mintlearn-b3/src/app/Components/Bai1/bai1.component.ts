1622f2837b74f6adf74b7feed65a21ddf438fab2
VanToan <toan.truong@kyanon.digital>
2018-12-04 15:00:50 +0700

    next to learn at home

--- LIVESTREAM/mintlearn-b3/src/app/Components/Bai1/bai1.component.ts
+++ LIVESTREAM/mintlearn-b3/src/app/Components/Bai1/bai1.component.ts
@@ -7,11 +7,15 @@ import { Component } from '@angular/core';
     <button (click)="CONG()">+</button>
      <button (click)="TRU()">-</button>
 
-     
+     <button (click)="Reload()">ABC</button>
+    <hr>
+
+    <img (mouseover)="DOIHINH($event)" (mouseout)="GUOC($event)" (click)="INFO($event)" src="{{hinh}}" />
+    <a (load)="Refresh()" href="https://beautybox.com.vn/" target="blank">BeautyBox</a>
     `,
 })
 
-export class Bai1Component{
+export class Bai1Component{  
     
     so:number = 0;
 
@@ -20,7 +24,36 @@ export class Bai1Component{
     }
 
     TRU(){
-        this.so = this.so - 1;
+        if(this.so == 0){
+            this.so = 0;
+        } else if(this.so > 0){
+            this.so = this.so - 1;
+        }
+       
     }
 
+    Refresh(){
+        location.reload();
+    }
+
+    Reload(){
+        window.setInterval('Refresh()',1000);
+    }
+    
+    
+
+
+  hinh:string = "https://beautybox.com.vn/media/wysiwyg/banner/47183141_303475523603567_3587191977365471232_n.jpg";
+    DOIHINH(e){
+        e.srcElement.src="https://beautybox.com.vn/media/wysiwyg/banner/46985260_2302493893316344_5124775511825842176_n.jpg";
+    }
+
+    GUOC(f){
+        f.srcElement.src = "https://beautybox.com.vn/media/wysiwyg/banner/47221069_209236476633566_4591702441837199360_n.jpg";
+    }
+
+    INFO(i){
+        console.log(i);
+    }
+    
 }
